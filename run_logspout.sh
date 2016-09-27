5c4314a544fea555e45afc881d33d4cbe7afb3b3
rstrazza <stz20ro1>
2016-09-26 23:26:31 -0400

    Adding elasticsearch plugins and utility scripts

--- run_logspout.sh
+++ run_logspout.sh
@@ -6,4 +6,4 @@ ENDPOINT="${LOCAL_IP}:5000"
 # Setup data
 data="environment=test"
 
-docker run -d --restart=always --name logspout --volume=/var/run/docker.sock:/var/run/docker.sock --publish=127.0.0.1:8000:80 gliderlabs/logspout "syslog+tcp://${ENDPOINT}?structured_data=${data}"
+docker run --restart=always --name logspout --volume=/var/run/docker.sock:/var/run/docker.sock --publish=127.0.0.1:8000:80 gliderlabs/logspout "syslog+tcp://${ENDPOINT}?structured_data=${data}"
